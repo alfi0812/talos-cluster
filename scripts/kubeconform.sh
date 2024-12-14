cacdc28f3cfa588a86ff65fc9a8a7ad411b22963
alfi0812 <admin@alfi0812.de>
2024-12-14 11:57:16 +0000

    f

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -32,16 +32,6 @@ done
 echo "=== Validating kustomizations in ${KUBERNETES_DIR}/ ==="
 find "${KUBERNETES_DIR}" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
 do
-    echo "This is a Test === Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | kubeconform "${kubeconform_args[@]}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        exit 1
-    fi
-done
-
-echo "Test2 === Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
-find "${KUBERNETES_DIR}" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
     kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
