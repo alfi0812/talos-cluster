8936ef9e6e92a546c374bc07c045ae9a69849302
alfi0812 <admin@alfi0812.de>
2024-12-14 11:51:32 +0000

    f

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -20,7 +20,7 @@ kubeconform_args=(
     "-verbose"
 )
 
-echo "=== Validating standalone manifests in ${KUBERNETES_DIR}/flux ==="
+echo "=== Validating standalone manifests in ${KUBERNETES_DIR} ==="
 find "${KUBERNETES_DIR}" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
 do
     kubeconform "${kubeconform_args[@]}" "${file}"
@@ -29,17 +29,17 @@ do
     fi
 done
 
-echo "=== Validating kustomizations in ${KUBERNETES_DIR}/flux ==="
+echo "=== Validating kustomizations in ${KUBERNETES_DIR}/ ==="
 find "${KUBERNETES_DIR}" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
 do
-    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
+    echo "This is a Test === Validating kustomizations in ${file/%$kustomize_config} ==="
     kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
         exit 1
     fi
 done
 
-echo "=== Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
+echo "Test2 === Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
 find "${KUBERNETES_DIR}" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
 do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
